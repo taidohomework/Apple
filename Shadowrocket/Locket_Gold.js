59f8ba33de5f3fa2284bcdb6fd4b905057a96432
Đinh Vạn Tài <vantaidinh04@gmail.com>
2024-08-09 17:08:35 +0700

    Add config shadowrocket
    Locket Gold

---
+++ Shadowrocket/Locket_Gold.js
@@ -0,0 +1,6 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="By: Dinh Van Tai";var locket02={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2004-07-21T00:00:00Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2004-07-21T00:00:00Z",purchase_date:"2004-07-21T00:00:00Z",store:"app_store"},locket01={grace_period_expires_date:null,purchase_date:"2004-07-21T00:00:00Z",product_identifier:"com.locket02.premium.yearly",expires_date:"2004-07-21T00:00:00Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(locket01.product_identifier=s,obj.subscriber.subscriptions[s]=locket02):obj.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,obj.subscriber.entitlements[e]=locket01}else obj.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,obj.subscriber.entitlements.pro=locket01;$done({body:JSON.stringify(obj)});
